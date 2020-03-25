10002d51a88729ebd3dfdd330b4a84b50215b1a3
Yinlia0 <gongjianyu1997@gmail.com>
2020-03-25 12:02:53 +0800

    Create leagueskin.py

---
+++ leagueskin.py
@@ -0,0 +1,26 @@
+import requests
+import os
+import shutil
+from download import download
+from lxml import etree
+
+
+def leagueskin():
+    url = 'http://leagueskin.net/p/download-mod-skin-2020-chn'
+    html = requests.get(url).text
+    skin_html = etree.HTML(html)
+    result = skin_html.xpath('//a[@id="link_download3"]/@href')
+    return result[0]
+
+def skin_download(skin_url):
+    desktop_dir = os.path.join(os.path.expanduser("~"), 'Desktop', 'skin')
+    if os.path.exists(desktop_dir):
+        print('删除skin目录')
+        shutil.rmtree(desktop_dir)
+    else:
+        print('无skin目录，跳过删除')
+    download(skin_url, desktop_dir, replace=True, kind="zip", progressbar=True)
+
+
+if __name__ == '__main__':
+    skin_download(leagueskin())
